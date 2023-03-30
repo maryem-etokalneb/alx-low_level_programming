4868a4b5542c60de4c6562c11885334ccf5e32b7
maryem-etokalneb <maryem.benlagot@gmail.com>
2023-03-30 13:55:05 -0700

    finish

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
+/**
+ * This file contains a program that demonstrates how to access
+ * elements of an array using pointer arithmetic.
+ *
+ *
+ * Entry point of the program.
+ *
+ * @return 0 if the program executed successfully, non-zero otherwise.
+ */
+
 int main(void)
 {
-int n;
-int a[5];
-int *p;
-a[2] = 1024;
-p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-*(p + 5) = 98;
-  /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+int a[] = {3, 7, 98, 6, 2};
+int *p = &a[0];
+printf("a[2] = %d\n", *(p + 2));
 return (0);
 }
